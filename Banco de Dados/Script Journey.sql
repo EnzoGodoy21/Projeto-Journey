2f4921cd2314ed78215cefc63bdc42252b9e0b39
Enzo Godoy <enzo.godoy@bandtec.com.br>
2021-05-23 18:09:26 -0300

    Adicionando a Opção de adicionar playlist

--- Banco de Dados/Script Journey.sql
+++ Banco de Dados/Script Journey.sql
@@ -9,3 +9,25 @@ create table Usuario
     login varchar(45),
     senha varchar(16)
 );
+
+create table musica 
+(
+	idMusica int primary key auto_increment,
+    nomeMusica varchar(50),
+    genero varchar(45),
+    autor varchar(45),
+    nomeArquivo varchar(60)
+);
+
+create table playlist 
+(
+	idPlaylist int primary key auto_increment,
+    nomePlaylist varchar(45)
+);
+
+drop table musica;
+select * from usuario;
+
+
+select * from playlist;
+truncate usuario;
